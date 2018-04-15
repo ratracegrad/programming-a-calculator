6110e4e9caf4debb911f2879592d52dd2ab8c74d
Jennifer Bland <jennifer.bland@sbdinc.com>
2018-04-15 10:16:13 -0400

    Move javascript file to its own file

---
+++ app.js
@@ -0,0 +1,65 @@
+let num1 = '';
+let num2 = '';
+let operator = '';
+let total = '';
+
+$(document).ready(function() {
+    $('button').on('click', function(e) {
+        let btn = e.target.innerHTML;
+        if (btn >= '0' && btn <= '9') {
+            handleNumber(btn);
+        } else {
+            handleOperator(btn);
+        }
+    });
+});
+
+function handleNumber(num) {
+    if (num1 === '') {
+        num1 = num;
+    } else {
+        num2 = num;
+    }
+    displayButton(num);
+}
+
+function handleOperator(oper) {
+    if (operator === '') {
+        operator = oper;
+    } else {
+        handleTotal();
+        operator = oper;
+    }
+}
+
+function handleTotal() {
+    console.log('x', operator);
+    switch (operator) {
+        case '+':
+            total = +num1 + +num2;
+            displayButton(total);
+            break;
+        case '-':
+            total = +num1 - +num2;
+            displayButton(total);
+            break;
+        case '/':
+            total = +num1 / +num2;
+            displayButton(total);
+            break;
+        case 'X':
+            total = +num1 * +num2;
+            displayButton(total);
+            break;
+    }
+    updateVariables();
+}
+
+function displayButton(btn) {
+    $('.calc-result-input').text(btn);
+}
+
+function updateVariables() {
+    num1 = total;
+    num2 = '';
+}
